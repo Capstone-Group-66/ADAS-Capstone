8a746dccb0dc844945ebbf2d7ff5b40f5995924b
jasonkeah <jasonkeah@cmail.carleton.ca>
2026-01-13 13:58:14 -0500

    Skeleton Structure for ViewModel Testing + dependencies

--- mobile-app/MobileApplication/app/src/main/java/com/example/testapp/ViewModel/ViewModel.kt
+++ mobile-app/MobileApplication/app/src/main/java/com/example/testapp/ViewModel/ViewModel.kt
@@ -17,42 +17,6 @@ import kotlinx.coroutines.flow.stateIn
 class ViewModel(
     repository: BleTickRepository
 ) : ViewModel() {
-    // Each badge has its own state
-    private val _status1 = MutableStateFlow(true)
-    val status1 = _status1.asStateFlow()
-
-    private val _status2 = MutableStateFlow(false)
-    val status2 = _status2.asStateFlow()
-
-    private val _status3 = MutableStateFlow(true)
-    val status3 = _status3.asStateFlow()
-
-    private val _status4 = MutableStateFlow(false)
-    val status4 = _status4.asStateFlow()
-
-    var sonarValue = mutableStateOf(0)
-        private set
-
-    // Example: methods that update values
-    fun setStatus1(value: Boolean) {
-        _status1.value = value
-    }
-
-    fun setStatus2(value: Boolean) {
-        _status2.value = value
-    }
-
-    fun setStatus3(value: Boolean) {
-        _status3.value = value
-    }
-
-    fun setStatus4(value: Boolean) {
-        _status4.value = value
-    }
-
-    fun updateValue(value: Int) {
-        sonarValue.value = value
-    }
 
     val driveState: StateFlow<updateUIstate> = repository.dashboardState
         .map { vehicleAlert ->
