47d5eaf7a211373878794cd1daeabf63ae5d741d
JohnG471 <john471g@gmail.com>
2026-01-15 15:50:56 -0500

    Added ego frame and created sample scenario in main

--- jetson-core/scripts/SensorFusion/EgoFrameKalmanFilter.cpp
+++ jetson-core/scripts/SensorFusion/EgoFrameKalmanFilter.cpp
@@ -3,15 +3,22 @@
 
 using namespace cv;
 
-int stateDim = 5; //x, y, vx, vy, w
-int measDim = 4; //x, y, vx, vy (assumes w dosent change)
-	
-class LinearKalanFilter{
-public:
-	
-	KalmanFilter kf = KalmanFilter();
+int stateDim = 5; //x, y, vx, vy, yaw
+int measDim = 4; //x, y, vx, vy (if we don't have yaw)
+//int measDim = 5; //x, y, vx, vy, yaw
 
-	LinearKalanFilter (Mat initialState) {
+class EgoFrameKalmanFilter{
+public:
+	KalmanFilter kf = KalmanFilter();
+	
+	/**
+	 * Constructor for EgoFrameKalmanFilter
+	 * 
+	 * Mat initialState: cv::Mat matrix that is the initial state of the EgoFrame
+	 * initialState should be a (5,1) matrix contining x, y, vx, vy, yaw
+	 * 
+	 */
+	EgoFrameKalmanFilter (Mat initialState) {
 		kf.init(stateDim, measDim, 0, CV_32F);
 		
 		float dt = 0.05; //Elapsed time
@@ -29,6 +36,14 @@ public:
 			0, 0, 1, 0, 0,
 			0, 0, 0, 1, 0);
 		
+		// If yaw exists
+		// kf.measurementMatrix = (Mat_<float>(measDim,stateDim) <<
+		// 	1, 0, 0, 0, 0,
+		// 	0, 1, 0, 0, 0,
+		// 	0, 0, 1, 0, 0,
+		// 	0, 0, 0, 1, 0,
+		// 	0, 0, 0, 0, 1);
+		
 		kf.statePost = initialState;
 	}
 	
@@ -39,19 +54,11 @@ public:
 		return prediction;
 	}
 	
-	Mat update(Mat measurement, float dt){		
+	Mat update(Mat measurement){		
 		//setIdentity(kf.processNoiseCov, Scalar::all(1e-4));
 		//setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
 		//setIdentity(kf.errorCovPost, Scalar::all(1));
 		
-		//Update dt for accuracy
-		kf.transitionMatrix = (Mat_<float>(stateDim,stateDim) <<
-			1, 0, dt, 0, 0,
-			0, 1, 0, dt, 0,
-			0, 0, 1, 0, 0,
-			0, 0, 0, 1, 0,
-			0, 0, 0, 0, 1);
-
 		Mat estimated = kf.correct(measurement);
 		std::cout << "Estimated: " << std::endl << estimated << std::endl << std::endl;
 		return estimated;
--- jetson-core/scripts/SensorFusion/main.cpp
+++ jetson-core/scripts/SensorFusion/main.cpp
@@ -1,26 +1,71 @@
 #include "opencv2/tracking.hpp"
 #include <iostream>
 #include "ObjectKalmanFilter.cpp"
+#include "EgoFrameKalmanFilter.cpp"
 
 using namespace cv;
 
+EgoFrameKalmanFilter egoFrame;
+
+int stateDim = 5;
+
+bool FCW_check(Mat ef, Mat track){
+	float ef_x = ef.at<float>(0,0);
+	float track_x = track.at<float>(0,0);
+	
+	// float ef_y = ef.at<float>(1,1);
+	// float track_y = track.at<float>(1,1);
+
+	float ef_vx = ef.at<float>(3,3);
+	float track_vx = track.at<float>(3,3);	
+
+	// float ef_vy = ef.at<float>(4,4);
+	// float track_vy = track.at<float>(4,4);
+	
+	float stop_time = ef_vx / (0.7 * 9.81) + 2.5;
+
+	return stop_time <= (track_x/track_vx);
+}
+
 int main(int, char**)
 {
-	Mat initialState = (Mat_<float>(stateDim,1) << 5, 5, 0, 0, 10);
-	ObjectKalmanFilter kf = ObjectKalmanFilter(initialState);
+	//Setup egoframe
+	Mat egoFrameInitialState = (Mat_<float>(stateDim,1) << 0, 0, 5, 0, 0); //Vehicle at 0,0 moving 5x,0y with 0 yaw
+	egoFrame = EgoFrameKalmanFilter(egoFrameInitialState);
+
+	//Setup object tracker
+	Mat trackInitialState = (Mat_<float>(stateDim,1) << 5, 5, 0, 0, 10); //Object at 5,5 not moving with a width of 10
+	ObjectKalmanFilter track1 = ObjectKalmanFilter(trackInitialState);
 	
 	int i = 0;
-	int x_position = 0;
-	int y_position = 0;
+
+	int ef_x = 0;
+	int ef_y = 0;
+	int ef_vx = 5;
+	int ef_vy = 0;
+
+	int t_x = 0;
+	int t_y = 0;
+	int t_vx = 5;
+	int t_vy = 0;
+
 	while (i < 10) {
-		kf.getPrediction();
+		Mat ef_prediction = egoFrame.getPrediction();
+		Mat track_prediction = track1.getPrediction();
+
+		FCW_check(ef_prediction, track_prediction);	
+
+		//Update egoFrame and track
+		Mat egoFrameMeas = (Mat_<float>(measDim, 1) << ef_x, ef_y, ef_vx, ef_vy);
+		Mat trackMeas = (Mat_<float>(measDim, 1) << t_x, t_y, t_vx, t_vy);
 		
-		Mat measurement = (Mat_<float>(measDim, 1) << x_position, y_position, 5, 3);
+		egoFrame.update(trackMeas);
+		track1.update(trackMeas);
 		
-		kf.update(measurement);
-		
+		//Manually change location of vehicle and objects
 		i++;
-		x_position += 5;
-		y_position += 3;
+		ef_x += 5;
+		t_x += 5;
+		t_vx -= 1; //Decrease object speed by 1 so that vehicle and object collide eventuall
 	}
 }
