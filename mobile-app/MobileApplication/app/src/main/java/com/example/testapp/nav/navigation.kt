4630734808005d263f2b12ddb2635f385f29bc8a
jasonkeah <jasonkeah@cmail.carleton.ca>
2026-01-15 20:56:04 -0500

    Klint

--- mobile-app/MobileApplication/app/src/main/java/com/example/testapp/nav/navigation.kt
+++ mobile-app/MobileApplication/app/src/main/java/com/example/testapp/nav/navigation.kt
@@ -1,17 +1,16 @@
 package com.example.testapp.nav
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.testapp.viewmodel.VehicleStatusViewModelFactory
 import com.example.testapp.model.BleTickRepository
 import com.example.testapp.screens.Drive
 import com.example.testapp.screens.home
 import com.example.testapp.screens.settings
 import com.example.testapp.viewmodel.VehicleStatusViewModel
+import com.example.testapp.viewmodel.VehicleStatusViewModelFactory
 
 @Composable
 fun Navigation(
