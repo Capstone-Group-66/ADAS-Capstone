599796faa0b92668c932e4a67567c7065f32dcfc
JohnG471 <john471g@gmail.com>
2026-03-17 17:35:14 -0400

    Added init expirire to vehicle alert

--- mobile-app/MobileApplication/app/src/main/java/com/example/testapp/model/VehicleAlertReducer.kt
+++ mobile-app/MobileApplication/app/src/main/java/com/example/testapp/model/VehicleAlertReducer.kt
@@ -16,6 +16,13 @@ object VehicleAlertReducer {
             detection = ObjectDetection.None,
             bsd = BlindSpotStatus(leftActive = false, rightActive = false),
             lastTickId = -1,
+            expiries = Expiries(
+                fcw = 0,
+                rcw = 0,
+                bsd_l = 0,
+                bsd_r = 0,
+                ldw = 0,
+            )
         )
 
     /**
